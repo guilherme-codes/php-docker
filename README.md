61ee6112012c4af8def767fd595be98e31d2a41f
guilherme cazaroto <gui.cazaroto@gmail.com>
2020-10-15 12:21:02 -0300

    lamp

---
+++ README.md
@@ -0,0 +1,11 @@
+# docker-lamp
+
+```
+docker-compose up -d
+```
+
+Run mysql client:
+
+- `docker-compose exec db mysql -u root -p` 
+
+Enjoy !
---
+++ www/index.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>Hello...</title>
+
+  <meta charset="utf-8"> 
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+    <?php echo "<h1>Hello World</h1>"; ?>
+    </div>
+</body>
+</html>
